df38b6054398165f9130ccf3b546b5d93799040c
Himadhar H <misterhimadhar@gmail.com>
2023-07-05 11:39:21 +0200

    updated for posts
    updated for posts

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -16,12 +17,36 @@ export default function Home() {
           <a href="https://nextjs.org/learn">the Next.js tutorial</a>.)
         </p>
         <p>
-          Try this link to the 
+          Try this link to the
           <Link href="/posts/first-post" className={utilStyles.colorInherit}>
             "First post"
           </Link>.
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blogs</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
